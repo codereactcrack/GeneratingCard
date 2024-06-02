08d148bb7f9af0f5bc51514ea4fb77fa1baf4ec2
CodeReactCrack <codereactcrack@gmail.com>
2024-06-02 15:51:14 +0530

    Added date picker using Material-UI

--- src/Form/GetDetails.jsx
+++ src/Form/GetDetails.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from "react-hook-form"
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 
 const GetDetails = () => {
     const {
@@ -8,56 +14,67 @@ const GetDetails = () => {
         watch,
         formState: { errors },
       } = useForm()
-    
-      const onSubmit = (data) => console.log(data)
-  return (
-    <div className='form-container'>
-        <form onSubmit={handleSubmit(onSubmit)}>
-    
-            <label>First Name -</label>
-            <input type='text' id='firstName'{...register("firstName" ,{ required: true, maxLength: 20 })} />
-            {errors.firstName && <span>This field is required</span>}
-            <br/>
 
-            <label>Last Name -</label>
-            <input type='text'id='lastName' {...register("lastName" ,{ maxLength: 20 })} />
-            <br/>
+      const defaultDate = Date();
+      const initialDate = dayjs(defaultDate);
+      const [selectedDate, setSelectedDate] = useState(initialDate);
 
-            <label>Designation -</label>
-            <input type='text' id='designation'{...register("designation", { required: true, maxLength: 20 })} />
-            {errors.designation && <span>This field is required</span>}
-            <br/>
+      const onSubmit = (data) => console.log(data,selectedDate.format('DD-MM-YYYY'))
 
-            <label>Department -</label>
-            <input type='text' id='department'{...register("department", { required: true, maxLength: 20 })} />
-            {errors.department && <span>This field is required</span>}
-            <br/>
-
-            <label>Email -</label>
-            <input type="email" id="email" {...register("email", { required: true})} />
-            {errors.email && <span>This field is required</span>}
-            <br/>
-
-            <label>Gender Selection -</label>
-            <select {...register("gender" ,{required:true})} >
-                <option value="female">female</option>
-                <option value="male">male</option>
-                <option value="other">other</option>
-            </select>
-            {errors.gender && <span>This field is required</span>}
-            <br/>
-
-            <label>Phone Number -</label>
-            <input type='text' id='phoneNo' {...register("phoneNo" ,{ required: true, maxLength: 10 ,minLength:10 })} />
-            {errors.phoneNo && <span>Correct Number is required</span>}
-            <br/>
-
+        return (
+            <div className='form-container'>
+                <form onSubmit={handleSubmit(onSubmit)}>
             
+                    <label>First Name -</label>
+                    <input type='text' id='firstName'{...register("firstName" ,{ required: true, maxLength: 20 })} />
+                    {errors.firstName && <span>This field is required</span>}
+                    <br/>
 
-            <input type="submit" />
-        </form>
-    </div>
-  )
+                    <label>Last Name -</label>
+                    <input type='text'id='lastName' {...register("lastName" ,{ maxLength: 20 })} />
+                    <br/>
+
+                    <label>Designation -</label>
+                    <input type='text' id='designation'{...register("designation", { required: true, maxLength: 20 })} />
+                    {errors.designation && <span>This field is required</span>}
+                    <br/>
+
+                    <label>Department -</label>
+                    <input type='text' id='department'{...register("department", { required: true, maxLength: 20 })} />
+                    {errors.department && <span>This field is required</span>}
+                    <br/>
+
+                    <label>Email -</label>
+                    <input type="email" id="email" {...register("email", { required: true})} />
+                    {errors.email && <span>This field is required</span>}
+                    <br/>
+
+                    <label>Gender Selection -</label>
+                    <select {...register("gender" ,{required:true})} >
+                        <option value="female">female</option>
+                        <option value="male">male</option>
+                        <option value="other">other</option>
+                    </select>
+                    {errors.gender && <span>This field is required</span>}
+                    <br/>
+
+                    <label>Phone Number -</label>
+                    <input type='text' id='phoneNo' {...register("phoneNo" ,{ required: true, maxLength: 10 ,minLength:10 })} />
+                    {errors.phoneNo && <span>Correct Number is required</span>}
+                    <br/>
+
+                    <label>D.O.B -</label>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <DemoContainer components={['DatePicker']}>
+                            <DatePicker value={selectedDate} onChange={(newDate)=> setSelectedDate(newDate)}/>
+                        </DemoContainer>
+                    </LocalizationProvider>
+                    <br/>
+
+                    <input type="submit" />
+                </form>
+            </div>
+        )
 }
 
 export default GetDetails
